be412c594a6e7fc42daadc7a67721bd63bd7310f
william <williamcg1227@gmail.com>
2023-07-19 15:23:41 -0300

    primeiro commit

---
+++ Variaveis/src/TestaPontoFlutuante.java
@@ -0,0 +1,6 @@
+public class TestaPontoFlutuante {
+	public static void main(String[] args) {
+		double salario = 618.50;
+		System.out.print("seu salario e " + salario + " kkkkkkkkkkkkk otario");
+	}
+}
---
+++ Variaveis/src/conversao.java
@@ -0,0 +1,8 @@
+
+public class conversao {
+	public static void main(String[] args) {
+		double salario = 618.5;
+		int novoSalario = (int) salario;
+		System.out.print(salario);
+	}
+}
